3495924f3f4b60c1f7d661c0f96b303892d25da1
marcobarroga <marcobarroga@protonmail.com>
2022-02-18 16:03:45 +0800

    init-stamp-for-file-db-table

---
+++ database/migrations/2022_02_18_072842_create_stamp_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStampTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stamp', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('fileRefId');
+            $table->unsignedBigInteger('user_id');
+            $table->double('x_coordinate');
+            $table->double('y_coordinate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stamp');
+    }
+}
